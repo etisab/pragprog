75b54624f2289e63e908cfd9d31fd385791ed44f
aria <ariasabeti@gmail.com>
2014-01-24 16:54:01 -0800

    restructured - removed redundant solutions

--- wq1/pragprog_L1.py
+++
@@ -1,7 +0,0 @@
-#using built-in python string slicing:  http://docs.python.org/release/2.3.5/whatsnew/section-slices.html
-def rev(s):
-	return s[::-1]
-
-#usage example
-s = "The quick brown fox jumps over the lazy dog.       "
-print(rev(s))
