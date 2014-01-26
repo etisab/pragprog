8495a5fd9cf850a0e628867eaf63ea168b9f3f93
aria <ariasabeti@gmail.com>
2014-01-26 03:25:29 -0800

    a more elegant implementation of fibonacci generator

--- wq2/challenge1/fib.py
+++ wq2/challenge1/fib.py
@@ -8,12 +8,10 @@ def fibGenerator(n):
 		i = j - i
 		yield i+j
 
+//can also implement without special cases
 def fibGeneratorInf():
 	i, j = 0, 1
-	
-	yield 0				#2 base cases to consider
-	yield 1
-	while True:			#resumes here for every call after base cases
-		j = i+j
-		i = j - i
-		yield i+j
+
+	while True:
+		yield i
+		i,j = j, i+j
