a6e0f09d4219e1f0cde96e89e3812448159a39c6
Aria <ariasabeti@gmail.com>
2014-10-21 10:24:48 -0700

    add solution for number of paths on nxn grid challenge

---
+++ wq5/challenge2.py
@@ -0,0 +1,9 @@
+#challenge 2 - write a function which returns the number of possible paths on an nxn grid.
+#Pascal's triangle strikes again!
+#http://math.stackexchange.com/questions/12093/counting-number-of-moves-on-a-grid
+
+from math import factorial as fac
+
+def numGridPaths(n):
+    n = 2*n
+    return fac(n)/(fac(2)*fac(n/2-1))
